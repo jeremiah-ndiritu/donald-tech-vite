e7120dbff3e980792ba07d862eea6b513c74701a
DigitalizedDev <digitalized.channel@gmail.com>
2025-10-15 17:07:39 +0300

    update image typoin index.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 let advertsSection = document.getElementById("adverts-section");
 
-let backendURL = "https://donald-tech-express.onrender.com";
+const backendURL = "https://donald-tech-express.onrender.com";
 
 fetch(`${backendURL}/api/adverts`)
   .then((res) => res.json())
@@ -11,7 +11,9 @@ fetch(`${backendURL}/api/adverts`)
       div.className =
         (i + 1) % 2 == 0 ? "advert-div desc-then-pic" : "advert-div";
       div.innerHTML = `
-            <img class="advert-pic" src="${ad.ig}" alt="Advert image${i + 1}" />
+            <img class="advert-pic" src="${ad.img}" alt="Advert image${
+        i + 1
+      }" />
             <div class="advert-desc">
                 <h3>${ad.h3}</h3>
                 <p>${ad.p}</h3>
